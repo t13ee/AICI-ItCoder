bb32882578f33777d690223aaf850692cdf7d0ee
t13ee <t13ee2005@gmail.com>
2024-04-04 15:28:43 +0700

    Data

---
+++ Data/Languages/lang-en.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="354"/>
+        <source>AICI-ItCoder</source>
+        <translation>AICI-ItCoder</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="314"/>
+        <source>Tiến Trình Mới</source>
+        <translation type="obsolete">New Progress</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="315"/>
+        <source>Mở Tiến Trình</source>
+        <translation type="obsolete">Open Process</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="328"/>
+        <source>Phần mềm hỗ trợ chèn mã vào ItCoder khi không cho sao chép và dán</source>
+        <translation>The software supports inserting code into ItCoder when copying and pasting is not allowed</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="330"/>
+        <source>CRE: T-13ee (DXT)</source>
+        <translation>CRE: T-13ee (DXT)</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="331"/>
+        <source>Hệ Thống</source>
+        <translation>System</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="332"/>
+        <source>Sáng</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="333"/>
+        <source>Tối</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="334"/>
+        <source>Tiếng Việt</source>
+        <translation>Vietnamese</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="335"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="336"/>
+        <source>Nhập nội dung ở đây</source>
+        <translation>Enter content here</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="337"/>
+        <source>Thời gian chờ:</source>
+        <translation>Waiting time:</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="345"/>
+        <source> giây</source>
+        <translation> second</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="339"/>
+        <source>Thời gian nghỉ</source>
+        <translation>Time delay</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="340"/>
+        <source>Cố định:</source>
+        <translation>Default:</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="342"/>
+        <source>Ngẫu nhiên:</source>
+        <translation>Random:</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="344"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="346"/>
+        <source>Mở Tệp</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="347"/>
+        <source>Bắt Đầu</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="348"/>
+        <source>Đường dẫn tệp</source>
+        <translation>File path</translation>
+    </message>
+    <message>
+        <location filename="Scr/ui_main.py" line="331"/>
+        <source>Tiến trình 1</source>
+        <translation type="obsolete">Progress 1</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="352"/>
+        <source>Thoát</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="Scr/ui_main.py" line="333"/>
+        <source>Thêm</source>
+        <translation type="obsolete">Add</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="353"/>
+        <source>Lưu</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="Scr/ui_main.py" line="335"/>
+        <source>Xóa Toàn Bộ</source>
+        <translation type="obsolete">Clear</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="351"/>
+        <source>Bảng tiến trình</source>
+        <translation>Progress panel</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="349"/>
+        <source>Tạm Dừng</source>
+        <translation>Stop</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="326"/>
+        <source>Tạo Mới</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="327"/>
+        <source>Mở</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="349"/>
+        <source>Hỗ trợ định dạng IDE ItCoder</source>
+        <translation type="obsolete">ItCoder IDE format support</translation>
+    </message>
+    <message>
+        <location filename="../../Scr/ui_main.py" line="350"/>
+        <source>Tự động làm đẹp mã nguồn trong IDE ItCoder</source>
+        <translation>Automatically beautify source code in ItCoder IDE</translation>
+    </message>
+</context>
+</TS>
